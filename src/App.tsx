169fb4e21259fa2ac0f1d4efdc1d44a500eddca4
williamwjd <william.jose.wjd@gmail.com>
2021-10-20 19:07:49 -0300

    salvando mensagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useContext } from 'react';
 import styled from './App.module.scss';
 import { LoginBox } from './components/LoginBox';
 import { MessageList } from './components/MessageList';
+import { SendMessageForm } from './components/SendMessageForm';
+import { AuthContext } from './contexts/Auth';
 
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <main className={styled.contentWrapper}>
       <MessageList/>
-      <LoginBox/>
+      {!!user ? <SendMessageForm/> : <LoginBox/>}
     </main>
   )
 }
